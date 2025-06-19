3af7026248419ad9879ee40135646817528b31f7
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-06-19 14:57:26 +0500

    Changes applied

--- src/lib/axiosIntance.ts
+++ src/lib/axiosIntance.ts
@@ -5,7 +5,7 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
-  },
+  }, 
 });
 
 // Add Authorization header to requests
